860cf46ae329c9488861add8cfd1fe3f45aebdb2
Alan Endersoy <aendersoy@gmail.com>
2024-05-27 10:30:10 +0200

    feat: Add possibility of fetching more shows

--- src/services/showService.ts
+++ src/services/showService.ts
@@ -6,16 +6,29 @@ const API_BASE_URL = 'https://api.tvmaze.com';
 const toast = useToast();
 
 /**
- * Fetches a list of shows from the TV Maze API.
- *
- * @returns A promise that resolves to an array of Show objects.
- * @throws An error if the request fails.
+ * Fetches TV shows from the TVMaze API.
+ * @param {number} numberOfPages - The number of pages to fetch. Defaults to 1 if not provided.
+ * @returns {Promise<Show[]>} - A promise that resolves to an array of shows.
  */
-export const fetchShows = async (): Promise<Show[]> => {
-  try {
-    const response = await axios.get<Show[]>(`${API_BASE_URL}/shows`);
-    toast.show('Shows loaded successfully!', 'success');
+export const fetchShows = async (
+  numberOfPages: number = 1
+): Promise<Show[]> => {
+  const fetchPage = async (page: number): Promise<Show[]> => {
+    const response = await axios.get<Show[]>(
+      `${API_BASE_URL}/shows?page=${page}`
+    );
     return response.data;
+  };
+
+  try {
+    const promises = [];
+    for (let i = 0; i < numberOfPages; i++) {
+      promises.push(fetchPage(i));
+    }
+
+    const results = await Promise.all(promises);
+    toast.show('Shows loaded successfully!', 'success');
+    return results.flat();
   } catch (error) {
     toast.show('Failed to fetch shows', 'error');
     throw new Error('Failed to fetch shows');
